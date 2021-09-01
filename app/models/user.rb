544e27029b535ec31705949d10d2efd15e39af33
sophie <justanothersophie@gmail.com>
2021-09-01 15:51:29 -0400

    users/login.erb, validates presence of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
     validates :username, presence: true, uniqueness: true
+    validates :email, presence: true
 
     has_many :messages
     has_many :topics, through: :messages
