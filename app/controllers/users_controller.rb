e0effa9fa2af20b352711cf5718ab47776f5753f
sophie <justanothersophie@gmail.com>
2021-09-01 16:35:57 -0400

    Filled out Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,60 @@
-class TopicsController < ApplicationController
+class UsersController < ApplicationController
 
+    get '/signup' do
+        if Helper.logged_in?(session)
+            flash[:message] = "You're already logged in!"
+            redirect '/topics'
+        end
+        @session = session
+        erb :'/users/create_user'
+    end
+
+    get '/login' do
+        if Helper.logged_in?(session)
+            flash[:message] = "You're already logged in!"
+            redirect '/topics'
+        end
+        @session = session
+        erb :'/users/login'
+    end
+
+    get '/logout' do
+        if !Helper.logged_in?(session)
+            flash[:message] = "You're already logged out!"
+            redirect '/login'
+        end
+        flash[:message] = "Logout successful! Goodbye, #{Helper.current_user(session).username}!"
+        session[:user_id] = nil
+        redirect '/login'
+    end
+
+    post '/login' do
+        user = User.find_by(username: params[user][:username])
+        if user && user.authenticate(params[user][:password])
+            session[:user_id] = user.id
+            session[:message] = "Login successful! Welcome back, #{Helper.current_user(session).username}!"
+            redirect '/topics'
+        else
+            flash[:message] = "Incorrect username or password. Login failed. Sorry!"
+            redirect '/login'
+        end
+    end
+
+    post '/signup' do
+        user = User.new(params[user])
+        if user.save
+            session[:user_id] = user.id
+            session[:message] = "Signup successful! Welcome, #{Helper.current_user(session).username}!"
+            redirect '/topics'
+        else
+            flash[:message] = "An error occurred. Signup failed."
+            redirect '/signup'
+        end
+    end
+
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        @session = session
+        erb :'/users/show'
+    end
 end
