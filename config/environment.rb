6d77492d3251894a9083b085de906392009b3ce0
sophie <justanothersophie@gmail.com>
2021-09-01 00:26:26 -0400

    Fixed broken database configuration, created database.yml, created database

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
-ENV['SINATRA_ENV'] ||= "development"
+ENV['SINATRA_ENV'] ||= "default"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/forum#{ENV['SINATRA_ENV']}.sqlite"
+  :database => "db/forum.sqlite"
 )
 
 require_all 'app'
