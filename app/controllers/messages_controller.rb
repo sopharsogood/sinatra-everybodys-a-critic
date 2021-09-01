f3d9f1d9a987cef524ef9a36d42f2eceae01ae5c
sophie <justanothersophie@gmail.com>
2021-09-01 19:03:38 -0400

    Routes for new, edit, and show .erb for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,40 @@
 class MessagesController < ApplicationController
 
+    get '/topics/:id/reply' do
+        if !Helper.logged_in?(session)
+            flash[:message] = "You must be logged in to post messages."
+            session[:failed_due_to_not_logged_in] = '/topics/' + params[:id].to_s + '/reply'
+            redirect '/login'
+        end
+        @topic = Topic.find_by(id: params[:id])
+        @session = session
+        erb :'/messages/new'
+    end
+
+    get '/messages/:id' do
+        @message = Message.find_by(id: params[:id])
+        @session = session
+        erb :'/messages/show'
+    end
+
+    get '/messages/:id/edit' do
+        if !Helper.logged_in?(session)
+            flash[:message] = "You must be logged in to edit messages."
+            session[:failed_due_to_not_logged_in] = '/messages/' + params[:id].to_s + '/edit'
+            redirect '/login'
+        end
+        @message = Message.find_by(id: params[:id])
+        if Helper.current_user(session) != @message.user
+            flash[:message] = "Only the original poster of a message can edit it."
+            redirect '/topics/' + params[:id]
+        end
+        if @message.topic.messages.first == @message
+            redirect '/topics/' + @message.topic.id.to_s + '/edit'
+        else
+            @session = session
+            erb :'/messages/edit'
+        end
+    end
+        
+
 end
