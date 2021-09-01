5ddf0b85d055dd7fa79b825b778cc343fbfde371
sophie <justanothersophie@gmail.com>
2021-09-01 16:25:31 -0400

    Added Topic 'bumping'

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,7 @@ class TopicsController < ApplicationController
 
     get '/topics' do
         @session = session
-        @topics = Topic.all
+        @topics = Topic.all.sort_by {|topic| topic.messages.last.created_at}
         erb :'/topics/index'
     end
 
