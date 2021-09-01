99d421e385d46e23c2c6c4648977729ed5fe0a20
sophie <justanothersophie@gmail.com>
2021-09-01 13:20:48 -0400

    User can convert to and find by slug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,26 @@ class User < ActiveRecord::Base
 
     has_many :messages
     has_many :topics, through: :messages
+
+    SLUG_CODEBOOK = {
+        "$" => "S",
+        "(" => "",
+        ")" => "",
+        "." => "",
+        "&" => "and",
+        " " => "-",
+        '"' => "",
+        "'" => "",
+        "+" => "plus"
+    }
+
+    def slug
+        self.username.gsub(/\W/,SLUG_CODEBOOK).downcase
+    end
+    
+    def self.find_by_slug(slug)
+        self.all.detect do |user|
+            user.slug == slug
+        end
+    end
 end
