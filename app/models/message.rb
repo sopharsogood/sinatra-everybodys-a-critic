ffc0adec72840d9b25e4cc51c385dcfa370c5034
sophie <justanothersophie@gmail.com>
2021-09-01 01:11:13 -0400

    Object relationships fixed to not say thread instead of topic

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ActiveRecord::Base
     belongs_to :user
-    belongs_to :thread
+    belongs_to :topic
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
     validates :username, presence: true, uniqueness: true
 
     has_many :messages
-    has_many :threads, through: :messages
+    has_many :topics, through: :messages
 end
